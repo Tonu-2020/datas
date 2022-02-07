c93f591eb8dca2238b1248ffdd5475b83b20ba2f
Tonu-2020 <74651951+Tonu-2020@users.noreply.github.com>
2022-02-07 15:19:24 +0530

    Add files via upload

---
+++ ds exam/number.py
@@ -0,0 +1,18 @@
+lst = []
+
+p = int(input("Enter size of phonenumber : "))
+print("enter the phonenumber")
+# iterating till the range
+for i in range(0, p):
+    ele = int(input())
+ 
+    lst.append(ele)
+    
+def absent_digits(lst):
+  all_nums = set([0,1,2,3,4,5,6,7,8,9])
+  lst = set([int(i) for i in lst])
+  lst = lst.symmetric_difference(all_nums)
+  lst = sorted(lst)
+  return lst
+print("missing number")
+print(absent_digits(lst))
---
+++ ds exam/s1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.title("student mark")
+plt.xlabel("total mark")
+plt.ylabel("mark obtained")
+x = np.array([10,20,30,40,50,60,70,80,90,100])
+x1 = [38,62,18,75,38,59,66,92,52,75]
+plt.scatter(x,x1, color = 'red', marker="*")
+
+y = [74,44,85,19,88,69,50,33,29,32]
+
+plt.scatter(x,y, marker="^")
+
+plt.legend(["Maths", "English"], loc ="upper right")
+plt.show()
+ 
